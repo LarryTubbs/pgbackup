e95bac2319e63f16ba69e084c6c17274605417d5
LarryTubbs <larry.tubbs@gmail.com>
2019-07-02 16:31:13 -0500

    tied it all together

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
-    name='pgpackup',
+    name='pgbackup',
     version='0.1.0',
     author='Larry Tubbs',
     author_email='larry.tubbs@gmail.com',
@@ -12,5 +12,13 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/larrytubbs/pgbackup',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ],
+    }
 )
+
